df4c7c59fec7d7699e4e67d3c39b7dfc37244728
River <1007941801@qq.com>
2022-07-31 22:07:38 +0800

    优化代码

--- index.js
+++ index.js
@@ -26,20 +26,21 @@ class AutoRunChromeExtPlugin {
               fs.constants.F_OK,
               async (err2) => {
                 if (!err2) {
-                  const { outputPath } = stats.compilation.compiler;
                   // 清空目标文件夹里的内容
                   fsExtra.emptyDirSync(this.targetDir);
-                  // 将打包后文件夹里的内容复制到目标文件夹里
+                  // 将打包后文件夹里的内容复制到目标文件夹
+                  const { outputPath } = stats.compilation.compiler;
                   fsExtra.copySync(outputPath, this.targetDir, {
                     dereference: true,
                   });
-                  // 打开Chrome浏览器的扩展管理页面
+
+                  // 构建driver
                   const webdriver = require("selenium-webdriver");
                   const remote = require("selenium-webdriver/remote");
                   const {
                     ServiceBuilder,
                   } = require("selenium-webdriver/chrome");
-                  const { Builder, By, until } = webdriver;
+                  const { Builder, By } = webdriver;
                   const driver = new Builder()
                     .forBrowser("chrome")
                     .setChromeService(
@@ -48,8 +49,10 @@ class AutoRunChromeExtPlugin {
                     .build();
                   driver.setFileDetector(new remote.FileDetector());
                   driver.manage().setTimeouts({ implicit: 2000 });
-                  await driver.get("chrome://extensions/");
+
                   // 打开Chrome浏览器的扩展管理页面
+                  await driver.get("chrome://extensions/");
+                  // 找到页面中开发者模式的按钮
                   const a = await driver
                     .findElement(By.css("extensions-manager"))
                     .getShadowRoot();
@@ -66,53 +69,63 @@ class AutoRunChromeExtPlugin {
                     const buttonStrip = await devDrawer.findElement(
                       By.id("buttonStrip")
                     );
+                    // 找到页面中加载已解压的扩展程序按钮
                     const loadUnpackedBtn = await buttonStrip.findElement(
                       By.id("loadUnpacked")
                     );
                     if (loadUnpackedBtn) {
                       // 判断autoClickSelect.exe文件是否存在
-                      fs.access(
-                        this.autoClickSelectLocation,
-                        fs.constants.F_OK,
-                        async (err) => {
-                          if (!err) {
-                            // 执行autoClickSelect.exe文件，等待自动点击选择文件按钮
-                            const exec = require("child_process").execFile;
-                            const child = exec(
-                              this.autoClickSelectLocation,
-                              [this.targetDir],
-                              {},
-                              (error, stdout, stderr) => {
-                                if (error) return console.error(error);
-                              }
-                            );
-                          } else {
-                            console.log(
-                              `所需的autoClickSelect.exe文件不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86`
-                            );
+                      if (
+                        this.autoClickSelectLocation &&
+                        typeof this.autoClickSelectLocation === "string"
+                      ) {
+                        fs.access(
+                          this.autoClickSelectLocation,
+                          fs.constants.F_OK,
+                          async (err) => {
+                            if (!err) {
+                              // 执行autoClickSelect.exe文件，等待自动点击选择文件按钮
+                              const exec = require("child_process").execFile;
+                              const child = exec(
+                                this.autoClickSelectLocation,
+                                [this.targetDir],
+                                {},
+                                (error, stdout, stderr) => {
+                                  if (error) return console.error(error);
+                                }
+                              );
+                            } else {
+                              console.log(
+                                `auto-load-chrome-ext-webpack-plugin所需的autoClickSelect.exe文件不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86`
+                              );
+                            }
+                            // 点击加载已解压的扩展程序按钮
+                            await loadUnpackedBtn.sendKeys(this.targetDir);
                           }
-                          // 点击加载已解压的扩展程序，并选中刚才创建好的目标文件夹
-                          await loadUnpackedBtn.sendKeys(this.targetDir);
-                        }
-                      );
+                        );
+                      } else {
+                        console.log(
+                          "请正确传入auto-load-chrome-ext-webpack-plugin所需的autoClickSelectLocation参数，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
+                        );
+                      }
                     }
                   }
                 } else {
                   console.log(
-                    "auto-load-chrome-ext-webpack-plugin插件必需的chromedriver.exe文件不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
+                    "auto-load-chrome-ext-webpack-plugin必需的chromedriver.exe文件不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
                   );
                 }
               }
             );
           } else {
             console.log(
-              "auto-load-chrome-ext-webpack-plugin插件必需的targetDir目标文件夹不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
+              "auto-load-chrome-ext-webpack-plugin必需的targetDir目标文件夹不存在，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
             );
           }
         });
       } else {
         console.log(
-          "请正确传入auto-load-chrome-ext-webpack-plugin插件所必需的targetDir参数，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
+          "请正确传入auto-load-chrome-ext-webpack-plugin所必需的targetDir参数，配置请参考文档: https://www.jianshu.com/p/4c854f6c6f86"
         );
       }
     });
